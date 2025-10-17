c6c0dbaf2771b72e11686b2e847c9fca4d1b521b
Cainã Costa <me@cfcosta.com>
2025-10-17 14:22:52 -0300

    feat(estoa-arbitrary): implement Arbitrary trait

--- crates/arbitrary/src/lib.rs
+++ crates/arbitrary/src/lib.rs
@@ -1,1 +1,43 @@
+pub use rand::{CryptoRng, RngCore};
+use rand::{
+    Rng,
+    distr::{SampleString, StandardUniform},
+};
 
+const STRING_MAX_LEN: usize = 1024;
+
+pub trait Arbitrary
+where
+    Self: Sized,
+{
+    fn arbitrary<R: RngCore + CryptoRng>(rng: &mut R) -> Self;
+
+    fn random() -> Self {
+        Self::arbitrary(&mut rand::rng())
+    }
+}
+
+impl Arbitrary for String {
+    fn arbitrary<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        let len = rng.random_range(0..=STRING_MAX_LEN);
+        StandardUniform.sample_string(rng, len)
+    }
+}
+
+impl Arbitrary for u32 {
+    fn arbitrary<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        rng.next_u32()
+    }
+}
+
+impl Arbitrary for u64 {
+    fn arbitrary<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        rng.next_u64()
+    }
+}
+
+impl Arbitrary for usize {
+    fn arbitrary<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
+        rng.random_range(0..usize::MAX)
+    }
+}
---
+++ crates/arbitrary/tests/test_arbitrary.rs
@@ -0,0 +1,22 @@
+use estoa_arbitrary::Arbitrary;
+
+struct User {
+    name: String,
+    followers: u32,
+    following: u64,
+}
+
+impl Arbitrary for User {
+    fn arbitrary<R: rand::RngCore + rand::CryptoRng>(rng: &mut R) -> Self {
+        Self {
+            name: String::arbitrary(rng),
+            followers: u32::arbitrary(rng),
+            following: u64::arbitrary(rng),
+        }
+    }
+}
+
+#[test]
+fn test_unconstrained_random() {
+    let _ = User::random();
+}
